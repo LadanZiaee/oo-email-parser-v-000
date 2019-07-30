196f2f6e555c84ecb11621c1e705061eab221d88
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-07-30 18:41:37 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,14 +1,13 @@
 class EmailParser
   attr_accessor :email 
-  @@parser = []
+ 
   
   def initialize(emails)
-    emails = email 
-    @@parser = emails 
+    @email = emails 
   end
   
   def parse 
-    @@parser
+    
   end
   
 end 
